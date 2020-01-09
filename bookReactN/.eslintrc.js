30774b6e887c6e0e13058208f2fd24e05d0f0d38
Anthony Mercadal <anthonymercadal@MacBook-Pro-de-Anthony.local>
2020-01-09 14:38:41 +0100

    reinit repo with repo and add lint airbnb

---
+++ bookReactN/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": [
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "tsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
